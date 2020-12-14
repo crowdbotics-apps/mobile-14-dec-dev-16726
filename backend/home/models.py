27eef0fc9f29c8afc8b1af311d033d30e06bb2b7
Crowdbotics <team@crowdbotics.com>
2020-12-14 08:21:37 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,49 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
+    hgjg = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_hgjg",
+    )
+    drrtare = models.GenericIPAddressField(
+        protocol="both",
+        unpack_ipv4=False,
+        null=True,
+        blank=True,
+    )
+    jhgjgjg = models.ManyToManyField(
+        "users.User",
+        blank=True,
+        related_name="customtext_jhgjgjg",
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +57,16 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
--- backend/mobile_14_dec_dev_16726/wsgi.py
+++ backend/mobile_14_dec_dev_16726/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_14_dec_dev_16726.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_14_dec_dev_16726.settings")
 
 application = get_wsgi_application()
